5f9803972545ada84470b58ebf1c5ca552249c20
aadarsh1503 <aadarshchauhan35@gmail.com>
2024-10-27 23:24:50 +0530

    burger

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change the output directory from 'dist' to 'build'
+    sourcemap: true, // Enable sourcemaps if needed for debugging
+    rollupOptions: {
+      // Customize Rollup options if necessary
+      output: {
+        manualChunks: {
+          // Separate vendor files, if needed
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
